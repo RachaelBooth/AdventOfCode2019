284b857c67ff2e6e45ca14c18245f905300dace9
Rachael Booth <rachael.booth@softwire.com>
2019-12-05 10:04:16 +0000

    Refactor intcode computer

---
+++ AdventOfCode2019/Day2/ProgramAlarm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2019.Shared;
+
+namespace AdventOfCode2019.Day2
+{
+    public class ProgramAlarm : Solver<int>
+    {
+        private List<int> initialProgramme;
+
+        public ProgramAlarm() : base(2)
+        {
+            initialProgramme = ReadInput().ToList();
+        }
+
+        public void SolvePartOne()
+        {
+            var result = new IntcodeComputer(initialProgramme).GetResult(12, 2);
+            Console.WriteLine(result);
+        }
+
+        public void SolvePartTwo()
+        {
+            var noun = 0;
+            while (noun < 100)
+            {
+                var verb = 0;
+                while (verb < 100)
+                {
+                    var result = new IntcodeComputer(initialProgramme).GetResult(noun, verb);
+                    if (result == 19690720)
+                    {
+                        // Done
+                        Console.WriteLine($"Noun: {noun}, Verb: {verb}, answer: {(100 * noun) + verb}");
+                        return;
+                    }
+
+                    verb++;
+                }
+
+                noun++;
+            }
+            Console.WriteLine("Not found");
+        }
+
+        protected override int ParseLine(string line)
+        {
+            return int.Parse(line);
+        }
+    }
+}
--- AdventOfCode2019/Shared/IntcodeComputer.cs
+++ AdventOfCode2019/Shared/IntcodeComputer.cs
@@ -1,56 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace AdventOfCode2019.Day2
+namespace AdventOfCode2019.Shared
 {
-    public class IntcodeComputer : Solver<int>
+    public class IntcodeComputer
     {
         private List<int> Programme;
 
-        public IntcodeComputer() : base(2) {}
-
-        public void SolvePartOne()
+        public IntcodeComputer(List<int> programme)
         {
-            SetUpProgramme(12, 2);
-            var result = GetResult();
-            Console.WriteLine(result);
+            Programme = programme;
         }
 
-        public void SolvePartTwo()
+        public int GetResult(int noun, int verb)
         {
-            var noun = 0;
-            while (noun < 100)
-            {
-                var verb = 0;
-                while (verb < 100)
-                {
-                    SetUpProgramme(noun, verb);
-                    var result = GetResult();
-                    if (result == 19690720)
-                    {
-                        // Done
-                        Console.WriteLine($"Noun: {noun}, Verb: {verb}, answer: {(100 * noun) + verb}");
-                        return;
-                    }
-
-                    verb++;
-                }
-
-                noun++;
-            }
-            Console.WriteLine("Not found");
-        }
-
-        private int GetResult()
-        {
+            SetUpProgramme(noun, verb);
             RunProgramme();
             return Programme[0];
         }
 
         private void SetUpProgramme(int noun, int verb)
         {
-            Programme = ReadInput().ToList();
             SetNoun(noun);
             SetVerb(verb);
         }
@@ -94,12 +64,6 @@ namespace AdventOfCode2019.Day2
 
                 instructionPointer += 4;
             }
-
-        }
-
-        protected override int ParseLine(string line)
-        {
-            return int.Parse(line);
         }
     }
 }
